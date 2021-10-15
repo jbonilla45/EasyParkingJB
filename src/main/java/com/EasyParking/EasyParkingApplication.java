aa6baf7feb066464e08f0ba1bd8bd69d628affe1
jbonilla45 <jorge.bonilla12@misena.edu.co>
2021-10-14 19:36:28 -0500

    Initial Configuration

---
+++ src/main/java/com/EasyParking/EasyParkingApplication.java
@@ -0,0 +1,13 @@
+package com.EasyParking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyParkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyParkingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/EasyParking/EasyParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EasyParking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyParkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
