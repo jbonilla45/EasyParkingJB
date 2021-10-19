d49b3cb56cfda0f16a48c63926895ac902cf8c82
jbonilla45 <jorge.bonilla12@misena.edu.co>
2021-10-19 18:43:44 -0500

    UPDATE

--- src/main/java/com/EasyParking/security/SecurityConfig.java
+++ src/main/java/com/EasyParking/security/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .roles("ADMIN","USER");
     }
 
-    /*@Override
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/editar/**", "/agregar/**", "/eliminar")
@@ -29,5 +29,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin()
                 .loginPage("/login");
-    }*/
+    }
 }
